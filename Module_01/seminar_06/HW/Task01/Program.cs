bf8fa604dd8821f6793b148f874e7059524619c3
nktrtmv <nvartemov@edu.hse.ru>
2021-10-03 00:36:30 +0300

    part of last HW in CW

---
+++ Module_01/seminar_06/HW/Task01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static double[] sinarr(int N)
+        {
+            double[] arr = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                double cursum;
+                double sum = 0;
+                int j = 1;
+                do
+                {
+                    cursum = sum;
+                    sum +=
+                } while (cursum != sum);
+
+                arr[i] = sum;
+            }
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
