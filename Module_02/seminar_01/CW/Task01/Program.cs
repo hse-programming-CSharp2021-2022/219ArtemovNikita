a5b3a5183f0afc5172150180d6cc9840c4b123e5
nktrtmv <nvartemov@edu.hse.ru>
2021-10-26 19:31:10 +0300

    added new CW

---
+++ Module_02/seminar_01/CW/Task01/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point1 = new Point2D();
+            Console.WriteLine(point1.X);
+            Console.WriteLine(point1.Y);
+            var point2 = new Point2D();
+            (point2.X, point2.Y) = (5, 5);
+            Point2D.PrintPoint(point2);
+            var point3 = new Point2D(3, 3);
+            Point2D.PrintPoint(point3);
+            Console.WriteLine(point1.Distance());
+            Console.WriteLine(point2.Distance(point3));
+            Point2D.PrintPoint(point3+point2);
+            Point2D.PrintPoint(point3+1);
+            Point2D.PrintPoint(1+point2);
+            Point2D.PrintPoint(point3-point2);
+            Console.WriteLine(point1.ToString());
+
+
+        }
+    }
+
+    class Point2D
+    {
+        public Point2D() {}
+
+        public Point2D(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        
+        
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public static void PrintPoint(Point2D point)
+        {
+            Console.WriteLine($"{point.X} {point.Y}");
+        }
+
+        public double Distance()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+        
+        public double Distance(Point2D point)
+        {
+            return Math.Sqrt((X-point.X) * (X-point.X) + (Y-point.Y * (Y-point.Y)));
+        }
+        
+        public static Point2D operator+(Point2D point1, Point2D point2)
+        {
+            return new Point2D(point1.X + point2.X, point1.Y + point2.Y);
+        }
+        
+        public static Point2D operator-(Point2D point1, Point2D point2)
+        {
+            return new Point2D(point1.X - point2.X, point1.Y - point2.Y);
+        }
+        
+        public static Point2D operator+(int val, Point2D point)
+        {
+            return new Point2D(val + point.X, val + point.Y);
+        }
+        
+        public static Point2D operator+(Point2D point, int val)
+        {
+            return new Point2D(val + point.X, val + point.Y);
+        }
+
+        public override string ToString()
+        {
+            return X + " " + Y;
+        }
+    }
+}
---
+++ Module_02/seminar_01/CW/Task02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pol = new RegularPolygon(6, 10);
+            RegularPolygon.PolygonData(pol);
+        }
+    }
+
+    internal class RegularPolygon
+    {
+        public RegularPolygon()
+        {
+            SidesCount = 1;
+            Radius = 0;
+        }
+        
+        public RegularPolygon(int sides, double radius)
+        {
+            SidesCount = sides;
+            Radius = radius;
+        }
+        
+        public int SidesCount { get; set; }
+        public double Radius { get; set; }
+
+        public double Perimetr
+        {
+            get { return CalcPerimetr(); }
+        }
+
+        public double Area
+        {
+            get { return CalcArea(); }
+        }
+
+        private double CalcPerimetr()
+        {
+            return SidesCount * 2 * Radius * Math.Sin((Math.PI / 2) / SidesCount);
+        }
+
+        private double CalcArea()
+        {
+            return Radius * Radius * SidesCount * Math.Tan((Math.PI / 2) / SidesCount);
+        }
+
+        public static void PolygonData(RegularPolygon pol)
+        {
+            Console.WriteLine($"Количество сторон: {pol.SidesCount}\n" +
+                              $"Радиус: {pol.Radius}\n" +
+                              $"Периметр: {pol.Perimetr}\n" +
+                              $"Площадь: {pol.Area}");
+        }
+    }
+}
