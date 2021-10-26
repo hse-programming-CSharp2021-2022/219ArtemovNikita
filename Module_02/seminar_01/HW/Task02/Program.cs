9c4f8a14ece19e3448ceebce60c24df7a2711cee
nktrtmv <nvartemov@edu.hse.ru>
2021-10-27 00:39:17 +0300

    added HW for sem1

---
+++ Module_02/seminar_01/HW/Task02/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var p1 = new Point(rnd.Next(1,100), rnd.Next(-100,1), "first point");
+            var p2 = new Point(rnd.Next(-51,0), rnd.Next(0,51), "second point");
+            var p3 = new Point(rnd.Next(-100, 100), rnd.Next(-100, 100), "third point");
+            var dict = new Dictionary<double, string>(3);
+            dict.Add(p1.R, p1.Name);
+            dict.Add(p2.R, p2.Name);
+            dict.Add(p3.R, p3.Name);
+            dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var item in dict)
+            {
+                if (item.Value == "first point")
+                    Point.PrintData(p1);
+                if (item.Value == "second point")
+                    Point.PrintData(p2);
+                if (item.Value == "third point")
+                    Point.PrintData(p3);
+            }
+
+
+
+        }
+    }
+
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        
+        public string Name { get; set; }
+
+        public Point(double x, double y, string name) { (X, Y, Name) = (x, y, name); }
+
+        public Point() { (X, Y, Name) = (0, 0, "None"); }
+
+        public double R { get { return CalcR(); } }
+
+        public double F { get { return CalcF(); } }
+
+        private double CalcR() => Math.Sqrt(Y * Y + X * X);
+
+        private double CalcF()
+        {
+            if (X > 0 && Y >= 0)
+                return Math.Atan(Y / X);
+            else if (X > 0 && Y < 0)
+                return Math.Atan(Y / X) + 2 * Math.PI;
+            else if (X < 0)
+                return Math.Atan(Y / X) + Math.PI;
+            else if (Y > 0 && X == 0)
+                return Math.PI / 2;
+            else if (X == 0 && Y < 0)
+                return 1.5 * Math.PI;
+            else if (X == 0 && Y == 0)
+                return 0;
+            return 0;
+        }
+
+        public static void PrintData(Point point) => Console.WriteLine($"x: {point.X}\ty: {point.Y}\tr: {point.R}\tf: {point.F}");
+
+        }
+}
---
+++ Module_02/seminar_01/HW/Task03/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task03
+{
+    class Program
+    {
+        // Оставил вывод информации о многоугольниках после ввода всех данных, чтобы сохранить окрашивание площадей.
+        static void Main(string[] args)
+        {
+            var polygons = new List<RegularPolygon>();
+            var area = new List<double>();
+            var j = 0;
+            while(true)
+            {
+                Console.Write($"enter count of sides of {j+1} polygon: ");
+                var sides = int.Parse(Console.ReadLine());
+                Console.Write($"enter radius of {j+1} polygon: ");
+                var rad = double.Parse(Console.ReadLine());
+                polygons.Add(new RegularPolygon(sides, rad));
+                area.Add(polygons[j].Area);
+                if (rad == 0 || sides == 0)
+                    break;
+                j++;
+            }
+
+            var n = polygons.Count;
+            for (var i = 0; i < n; i++)
+            {
+                if (area[i] == area.Min())
+                {
+                    Console.Write($"{i+1} polygon data: ");
+                    RegularPolygon.GreenPolygonData(polygons[i]);
+                }
+                else if (area[i] == area.Max())
+                {
+                    Console.Write($"{i+1} polygon data: ");
+                    RegularPolygon.RedPolygonData(polygons[i]);
+                }
+                else
+                {
+                    Console.Write($"{i+1} polygon data: ");
+                    RegularPolygon.PolygonData(polygons[i]);
+                }
+            }
+        }
+    }
+
+    internal class RegularPolygon
+    {
+        public RegularPolygon()
+        {
+            SidesCount = 0;
+            Radius = 0;
+        }
+        
+        public RegularPolygon(int sides, double radius)
+        {
+            SidesCount = sides;
+            Radius = radius;
+        }
+
+
+        public int SidesCount { get; set; }
+        public double Radius { get; set; }
+
+        public double Perimetr
+        {
+            get { return CalcPerimetr(); }
+        }
+
+        public double Area
+        {
+            get { return CalcArea(); }
+        }
+
+        private double CalcPerimetr()
+        {
+            if (SidesCount == 0 || SidesCount == 1)
+                return 0;
+            return SidesCount * 2 * Radius * Math.Sin((Math.PI / 2) / SidesCount);
+        }
+
+        private double CalcArea()
+        {
+            if (SidesCount == 0 || SidesCount == 1)
+                return 0;
+            return Radius * Radius * SidesCount * Math.Tan((Math.PI / 2) / SidesCount);
+        }
+
+        public static void PolygonData(RegularPolygon pol)
+        {
+            Console.WriteLine($"Количество сторон: {pol.SidesCount}; " +
+                              $"Радиус: {pol.Radius}; " +
+                              $"Периметр: {pol.Perimetr}; " +
+                              $"Площадь: {pol.Area}");
+        }
+
+        public static void GreenPolygonData(RegularPolygon pol)
+        {
+            Console.Write($"Количество сторон: {pol.SidesCount}; " +
+                              $"Радиус: {pol.Radius}; " +
+                              $"Периметр: {pol.Perimetr}; ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Площадь: {pol.Area}");
+            Console.ResetColor();
+        }
+        
+        public static void RedPolygonData(RegularPolygon pol)
+        {
+            Console.Write($"Количество сторон: {pol.SidesCount}; " +
+                              $"Радиус: {pol.Radius}; " +
+                              $"Периметр: {pol.Perimetr}; ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Площадь: {pol.Area}");
+            Console.ResetColor();
+        }
+    }
+}
