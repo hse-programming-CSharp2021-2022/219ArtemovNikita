b9dadb73a1f0c544d97116132172bdc92b56dfdb
nktrtmv <nvartemov@edu.hse.ru>
2021-11-02 18:57:40 +0300

    added hw

---
+++ Module_02/seminar_02/CW/Task01/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new MyComplex(3, 4);
+            var c2 = new MyComplex(4, 3);
+            Console.WriteLine(c1 + c2);
+            Console.WriteLine(c1 - c2);
+            Console.WriteLine(c1 * c2);
+            Console.WriteLine(c1 / c2);
+        }
+    }
+
+    class MyComplex
+    {
+        private double re, im;
+
+        public double Re
+        {
+            get { return re; }
+            set { re = value; }
+        }
+
+        public double Im
+        {
+            get { return im; }
+            set { im = value; }
+        }
+
+        public MyComplex(double xre, double xim) { re = xre; im = xim; }
+        
+        public static MyComplex operator --(MyComplex mc)
+        { return new MyComplex(mc.re - 1, mc.im - 1); }
+
+        public static MyComplex operator ++(MyComplex mc)
+        { return new MyComplex(mc.re + 1, mc.im + 1); }
+
+        public double Mod() { return Math.Abs(re * re + im * im); }
+
+        static public bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+
+        static public bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+
+        public static MyComplex operator +(MyComplex c1, MyComplex c2)
+        {
+            return new MyComplex(c1.re + c2.re, c1.im + c2.im);
+        }
+        public static MyComplex operator -(MyComplex c1, MyComplex c2)
+        {
+            return new MyComplex(c1.re - c2.re, c1.im - c2.im);
+        }
+        public static MyComplex operator *(MyComplex c1, MyComplex c2)
+        {
+            return new MyComplex(c1.re*c2.re - c1.im*c2.im, c1.im*c2.re + c2.im*c1.re);
+        }
+        public static MyComplex operator /(MyComplex c1, MyComplex c2)
+        {
+            double a = c1.re;
+            double b = c1.im;
+            double c = c2.re;
+            double d = c2.im;
+            return new MyComplex((a*c + b*d) / (c*c + d*d),
+                (b*c - a*d) / (c*c + d*d));
+        }
+        public override string ToString()
+        {
+            return $"{re} + {im}i";
+        }
+    }
+}
---
+++ Module_02/seminar_02/HW/Task01/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main( )
+        {
+            var sym1 = new ConsolePlate('X', ConsoleColor.White, ConsoleColor.Red);
+            var sym2 = new ConsolePlate('O', ConsoleColor.White, ConsoleColor.Magenta);
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n) || n < 2 || n > 35);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Console.ForegroundColor = sym1.PlateColor;
+                            Console.BackgroundColor = sym1.BackColor;
+                            Console.Write(sym1.PlateChar);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = sym2.PlateColor;
+                            Console.BackgroundColor = sym2.BackColor;
+                            Console.Write(sym2.PlateChar);
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 != 0)
+                        {
+                            Console.ForegroundColor = sym1.PlateColor;
+                            Console.BackgroundColor = sym1.BackColor;
+                            Console.Write(sym1.PlateChar);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = sym2.PlateColor;
+                            Console.BackgroundColor = sym2.BackColor;
+                            Console.Write(sym2.PlateChar);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+
+    
+    public class ConsolePlate
+    {
+        char _plateChar; 
+        ConsoleColor _plateColor = ConsoleColor.White;
+        private ConsoleColor _backColor = ConsoleColor.Black;
+        
+
+        public ConsolePlate()  
+        {
+            _plateChar = 'A';
+        }
+        public ConsolePlate(char plateChar, ConsoleColor plateColor, ConsoleColor backColor)
+        {
+            if (plateColor == backColor)
+            {
+                Console.WriteLine("Ошибка! Цвета заднего и переднего плана не должны совпадать!");
+                return;
+            }
+            PlateChar = plateChar;
+            PlateColor = plateColor;
+            BackColor = backColor;
+        }
+        public char PlateChar
+        {
+            set
+            {
+                if (value >= 65 || value <= 90) 
+                    _plateChar = value;
+                else
+                    _plateChar = 'A';
+            }
+            get { return _plateChar; }
+        }
+        public ConsoleColor PlateColor
+        {
+            set { _plateColor = value; }
+            get { return _plateColor; }
+        }
+
+        public ConsoleColor BackColor
+        {
+            get { return _backColor; }
+            set { _backColor = value; }
+        }
+
+    }
+
+}
